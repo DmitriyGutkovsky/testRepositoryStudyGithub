5b48bf382e7b82d739b78ec84aa545f40753d5d0
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-09-20 15:35:52 +0300

    commit in dev before task1

--- src/main/java/com/noirix/Main.java
+++ src/main/java/com/noirix/Main.java
@@ -20,5 +20,7 @@ public class Main {
     System.out.println("dev: string for cherry pick");
 
     System.out.println("dev changes: merge 2 branch");
+
+    System.out.println("commit in dev before task1");
   }
 }
