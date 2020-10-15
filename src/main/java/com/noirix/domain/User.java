faafb260d8420e19358e8ed773abda048dd566f9
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-10-15 11:30:30 +0300

    Lombok added Database properties file added was modified class User

--- src/main/java/com/noirix/domain/User.java
+++ src/main/java/com/noirix/domain/User.java
@@ -1,5 +1,6 @@
 package com.noirix.domain;
 
+import lombok.*;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
@@ -7,118 +8,33 @@ import java.sql.Timestamp;
 import java.util.Date;
 import java.util.Objects;
 
+//@Data
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private Long id;
+
     private String name;
+
     private String surname;
+
     private Date birthDate;
+
     private Gender gender = Gender.NOT_SELECTED;
+
     private Timestamp created;
+
     private Timestamp changed;
+
     private Float weight;
 
-  public User() {
-  }
+//  @Override
+//  public String toString() {
+//    return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
+//  }
 
-  public User(Long id, String name, String surname,
-              Date birthDate, Gender gender, Timestamp created,
-              Timestamp changed, Float weight) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.birthDate = birthDate;
-        this.gender = gender;
-        this.created = created;
-        this.changed = changed;
-        this.weight = weight;
-    }
-
-  public Long getId() {
-    return id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String getSurname() {
-    return surname;
-  }
-
-  public Date getBirthDate() {
-    return birthDate;
-  }
-
-  public Gender getGender() {
-    return gender;
-  }
-
-  public Timestamp getCreated() {
-    return created;
-  }
-
-  public Timestamp getChanged() {
-    return changed;
-  }
-
-  public Float getWeight() {
-    return weight;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public void setSurname(String surname) {
-    this.surname = surname;
-  }
-
-  public void setBirthDate(Date birthDate) {
-    this.birthDate = birthDate;
-  }
-
-  public void setGender(Gender gender) {
-    this.gender = gender;
-  }
-
-  public void setCreated(Timestamp created) {
-    this.created = created;
-  }
-
-  public void setChanged(Timestamp changed) {
-    this.changed = changed;
-  }
-
-  public void setWeight(Float weight) {
-    this.weight = weight;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    User user = (User) o;
-    return Objects.equals(id, user.id) &&
-            Objects.equals(name, user.name) &&
-            Objects.equals(surname, user.surname) &&
-            Objects.equals(birthDate, user.birthDate) &&
-            gender == user.gender &&
-            Objects.equals(created, user.created) &&
-            Objects.equals(changed, user.changed) &&
-            Objects.equals(weight, user.weight);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name, surname, birthDate, gender, created, changed, weight);
-  }
-
-  @Override
-  public String toString() {
-    return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
-  }
 }
