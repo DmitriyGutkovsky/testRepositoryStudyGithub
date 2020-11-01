dcd50c9fa1e2e863c538946d7b8df9b62f85e9a6
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-11-01 22:32:54 +0300

    AOP added: testing annotations @Before and @AfterReturning

--- src/main/java/com/noirix/aspect/LoggingAspect.java
+++ src/main/java/com/noirix/aspect/LoggingAspect.java
@@ -1,10 +1,9 @@
 package com.noirix.aspect;
 
 import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,15 +11,15 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
     private static final Logger log = Logger.getLogger(LoggingAspect.class);
 
-//    @Before("aroundRepositoryPointcut()")
-//    public void logBefore(JoinPoint joinPoint) {
-//        log.info("Method " + joinPoint.getSignature().getName() + " start");
-//    }
-//
-//    @AfterReturning(pointcut = "aroundRepositoryPointcut()")
-//    public void doAccessCheck(JoinPoint joinPoint) {
-//        log.info("Method " + joinPoint.getSignature().getName() + " finished");
-//    }
+    @Before("aroundRepositoryPointcut()")
+    public void logBefore(JoinPoint joinPoint) {
+        log.info("Method " + joinPoint.getSignature().getName() + " start from before advice");
+    }
+
+    @AfterReturning(pointcut = "aroundRepositoryPointcut()")
+    public void doAccessCheck(JoinPoint joinPoint) {
+        log.info("Method " + joinPoint.getSignature().getName() + " finished from after advice");
+    }
 
 //    @Pointcut("execution(* com.noirix.repository.impl.*Impl.*(..))")
     @Pointcut("execution(* com.noirix.repository.impl.UserRepositoryJdbcTemplateImpl.*(..))")
