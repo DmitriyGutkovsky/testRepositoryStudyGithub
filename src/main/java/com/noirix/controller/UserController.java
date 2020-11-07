3c1ffcb92b42155799071c04728ac5057be2e23c
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-11-07 19:14:05 +0300

    Spring MVC: UserController: methods getAllUsers and search

--- src/main/java/com/noirix/controller/UserController.java
+++ src/main/java/com/noirix/controller/UserController.java
@@ -1,18 +1,47 @@
 package com.noirix.controller;
 
+import com.noirix.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Collections;
-
 @Controller
+@RequestMapping("/users") // all methods mapping will start with "/users"
+@RequiredArgsConstructor
 public class UserController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public ModelAndView getHelloPage(){
-        return new ModelAndView("goodbye", Collections.singletonMap(
-                "someMessage", "Spring MVC is working!"));
+    public final UserService userService;
+
+    public static final String USER_PAGE = "users";
+    public static final String USERS_LIST_ATTRIBUTE = "users";
+
+    // will be called by URL: /users and RequestMethod.GET
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView getAllUsers(){
+
+        ModelAndView result = new ModelAndView();
+        result.setViewName(USER_PAGE);
+        result.addObject(USERS_LIST_ATTRIBUTE, userService.findAll());
+
+        return  result;
     }
+    // will be called by URL: /users/search and RequestMethod.GET
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public ModelAndView search(@RequestParam String query){
+
+        ModelAndView result = new ModelAndView();
+        result.setViewName(USER_PAGE);
+        result.addObject(USERS_LIST_ATTRIBUTE, userService.search(query));
+
+        return  result;
+    }
+
+
+
+
+
+
 }
