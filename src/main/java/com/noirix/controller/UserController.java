21c31c9824859422a1b5557bbb0a4936137a6a3e
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-11-08 11:43:52 +0300

    Spring MVC: UserController: replacing @RequestMapping(method = RequestMethod.GET) to @GetMapping for two meth ods: getAllUsers() and search()

--- src/main/java/com/noirix/controller/UserController.java
+++ src/main/java/com/noirix/controller/UserController.java
@@ -4,10 +4,7 @@ import com.noirix.controller.requests.SearchCriteria;
 import com.noirix.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.stream.Collectors;
@@ -23,7 +20,8 @@ public class UserController {
     public static final String USERS_LIST_ATTRIBUTE = "users";
 
     // will be called by URL: /users and RequestMethod.GET
-    @RequestMapping(method = RequestMethod.GET)
+//    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public ModelAndView getAllUsers(){
 
         ModelAndView result = new ModelAndView();
@@ -33,10 +31,10 @@ public class UserController {
         return  result;
     }
     // will be called by URL: /users/search and RequestMethod.GET
-    @RequestMapping(value = "/search", method = RequestMethod.GET)
+//    @RequestMapping(value = "/search", method = RequestMethod.GET)
 //    public ModelAndView search(@RequestParam ("query") String queryParam,
 //                               @RequestParam ("limit") Long limit ){
-
+    @GetMapping(value = "/search")
     public ModelAndView search(@ModelAttribute SearchCriteria criteria){
 
         ModelAndView result = new ModelAndView();
