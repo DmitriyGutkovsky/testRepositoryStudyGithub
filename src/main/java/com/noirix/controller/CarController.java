384a41aa6e72dc1e18e572d9ccb83524ce292cfc
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-11-08 17:18:06 +0300

    Spring MVC: CarController: implemented method getById()

--- src/main/java/com/noirix/controller/CarController.java
+++ src/main/java/com/noirix/controller/CarController.java
@@ -4,8 +4,11 @@ import com.noirix.service.CarService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collections;
 
 @Controller
 @RequestMapping("/cars")
@@ -23,6 +26,14 @@ public class CarController {
         ModelAndView result = new ModelAndView();
         result.setViewName(CARS_PAGE);
         result.addObject(CARS_LIST_ATTRIBUTE, carService.findAll());
+        return result;
+    }
+
+    @GetMapping(value = "/{carId}")
+    public ModelAndView getById(@PathVariable Long carId){
+        ModelAndView result = new ModelAndView();
+        result.setViewName(CARS_PAGE);
+        result.addObject(CARS_LIST_ATTRIBUTE, Collections.singletonList(carService.findById(carId)));
         return result;
     }
 
