f59bb49823bb0f9eadb5f4fc033445de5194c07d
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-11-08 17:30:42 +0300

    Spring MVC: CarController: implemented method search() with @RequestMapping and @RequestParam

--- src/main/java/com/noirix/controller/CarController.java
+++ src/main/java/com/noirix/controller/CarController.java
@@ -3,39 +3,52 @@ package com.noirix.controller;
 import com.noirix.service.CarService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Collections;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/cars")
 @RequiredArgsConstructor
 public class CarController {
 
-    public final CarService carService;
+  public final CarService carService;
 
-    public static final String CARS_PAGE = "cars";
+  public static final String CARS_PAGE = "cars";
 
-    public static final String CARS_LIST_ATTRIBUTE = "cars";
+  public static final String CARS_LIST_ATTRIBUTE = "cars";
 
-    @GetMapping
-    public ModelAndView getAllCars(){
-        ModelAndView result = new ModelAndView();
-        result.setViewName(CARS_PAGE);
-        result.addObject(CARS_LIST_ATTRIBUTE, carService.findAll());
-        return result;
-    }
+  @GetMapping
+  public ModelAndView getAllCars() {
+    ModelAndView result = new ModelAndView();
+    result.setViewName(CARS_PAGE);
+    result.addObject(CARS_LIST_ATTRIBUTE, carService.findAll());
+    return result;
+  }
 
-    @GetMapping(value = "/{carId}")
-    public ModelAndView getById(@PathVariable Long carId){
-        ModelAndView result = new ModelAndView();
-        result.setViewName(CARS_PAGE);
-        result.addObject(CARS_LIST_ATTRIBUTE, Collections.singletonList(carService.findById(carId)));
-        return result;
-    }
+  @GetMapping(value = "/{carId}")
+  public ModelAndView getById(@PathVariable Long carId) {
+    ModelAndView result = new ModelAndView();
+    result.setViewName(CARS_PAGE);
+    result.addObject(CARS_LIST_ATTRIBUTE, Collections.singletonList(carService.findById(carId)));
+    return result;
+  }
 
+  @RequestMapping(value = "/search", method = RequestMethod.GET)
+  public ModelAndView searchCar(
+      @RequestParam("query") String queryParam, @RequestParam("limit") Long limit) {
 
+    ModelAndView result = new ModelAndView();
+    result.setViewName(CARS_PAGE);
+    result.addObject(
+        CARS_LIST_ATTRIBUTE,
+        carService.search(queryParam)
+                .stream()
+                .limit(limit)
+                .collect(Collectors.toList()));
+
+    return result;
+  }
 }
