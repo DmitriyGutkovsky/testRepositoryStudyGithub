4dc53d3e65d6e23ddff1a5f831ac9e7a6c460d8d
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-11-11 20:29:06 +0300

    Spring REST Initial: implemented method savingUser()

--- src/main/java/com/noirix/controller/UserRestController.java
+++ src/main/java/com/noirix/controller/UserRestController.java
@@ -1,6 +1,7 @@
 package com.noirix.controller;
 
 import com.noirix.controller.requests.SearchCriteria;
+import com.noirix.controller.requests.UserCreateRequest;
 import com.noirix.domain.User;
 import com.noirix.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @RestController
@@ -37,6 +39,24 @@ public class UserRestController {
     @ResponseStatus(HttpStatus.OK)
     public List<User> userSearch(@ModelAttribute SearchCriteria searchCriteria){
         return userService.search(searchCriteria.getQuery());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User savingUser(@RequestBody UserCreateRequest userCreateRequest){
+
+        User user = new User();
+
+        user.setGender(userCreateRequest.getGender());
+        user.setName(userCreateRequest.getName());
+        user.setSurname(userCreateRequest.getSurname());
+        user.setBirthDate(userCreateRequest.getBirthDate());
+        user.setWeight(userCreateRequest.getWeight());
+        user.setCreated(new Timestamp(System.currentTimeMillis()));
+        user.setChanged(new Timestamp(System.currentTimeMillis()));
+
+        return userService.save(user);
+
     }
 
 }
