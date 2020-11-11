dbfc1282e7041eb1fcd866f3bae0afc329555651
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-11-11 20:14:07 +0300

    Spring REST Initial: implemented methods findUserById() & userSearch()

--- src/main/java/com/noirix/controller/UserRestController.java
+++ src/main/java/com/noirix/controller/UserRestController.java
@@ -1,13 +1,12 @@
 package com.noirix.controller;
 
+import com.noirix.controller.requests.SearchCriteria;
 import com.noirix.domain.User;
 import com.noirix.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,9 +17,26 @@ public class UserRestController {
 
     public final UserService userService;
 
+    //http://localhost:8080/rest/users
     @GetMapping
     public ResponseEntity<List<User>> findAllUsers(){
 //        return ResponseEntity.ok(userService.findAll());
         return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
     }
+
+
+    //http://localhost:8080/rest/users/5
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public User findUserById(@PathVariable Long id){
+        return userService.findById(id);
+    }
+
+    // http://localhost:8080/rest/users/search?query=Viachaslau
+    @GetMapping("/search")
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> userSearch(@ModelAttribute SearchCriteria searchCriteria){
+        return userService.search(searchCriteria.getQuery());
+    }
+
 }
