2af589c216d624dc561bb342fd98ace3cb567b7f
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-11-29 18:46:32 +0300

    Spring Boot Security configuration: Swagger: testing @ApiOperation

--- src/main/java/com/noirix/security/controller/RegistrationController.java
+++ src/main/java/com/noirix/security/controller/RegistrationController.java
@@ -3,6 +3,7 @@ package com.noirix.security.controller;
 import com.noirix.controller.requests.UserCreateRequest;
 import com.noirix.domain.User;
 import com.noirix.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +26,7 @@ public class RegistrationController {
 
     private  final PasswordEncoder passwordEncoder;
 
+    @ApiOperation(value = "Endpoint for registration users by 6 params")
     @PostMapping
     public ResponseEntity<Map<String, Object>> registration(@RequestBody UserCreateRequest userCreateRequest){
 
