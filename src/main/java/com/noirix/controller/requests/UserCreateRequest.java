8cf47a82dab5f9255970df3e017a7a64692f7565
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-11-29 17:31:28 +0300

    Spring Boot Security configuration: implemented RegistrationController - new user registration

--- src/main/java/com/noirix/controller/requests/UserCreateRequest.java
+++ src/main/java/com/noirix/controller/requests/UserCreateRequest.java
@@ -12,6 +12,10 @@ public class UserCreateRequest {
 
     private String surname;
 
+    private String login;
+
+    private String password;
+
     private Date birthDate;
 
     private Gender gender = Gender.NOT_SELECTED;
