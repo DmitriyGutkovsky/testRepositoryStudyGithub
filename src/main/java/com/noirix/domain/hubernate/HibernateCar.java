806058f9e4eac8ebf4a2b3ea1f5d6b083a558a84
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-12-01 21:54:37 +0300

    Hibernate: implemented mapping for table m_cars

---
+++ src/main/java/com/noirix/domain/hubernate/HibernateCar.java
@@ -0,0 +1,35 @@
+package com.noirix.domain.hubernate;
+
+import lombok.Data;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "m_cars")
+public class HibernateCar {
+
+    @Id
+    private Long id;
+
+    @Column
+    private String model;
+
+    @Column
+    private  int creationYear;
+
+    @Column
+    private Long userId;
+
+    @Column
+    private Double price;
+
+    @Column
+    private String color;
+
+}
