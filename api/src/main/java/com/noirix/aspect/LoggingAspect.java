0c44b4d035c6f83f3f3a7ee35e331fa1f0a9e006
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2021-01-12 21:28:21 +0300

    imports improved

--- api/src/main/java/com/noirix/aspect/LoggingAspect.java
+++ api/src/main/java/com/noirix/aspect/LoggingAspect.java
@@ -3,7 +3,11 @@ package com.noirix.aspect;
 import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
