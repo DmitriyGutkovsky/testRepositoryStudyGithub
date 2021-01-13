3115b6a62c4707f26a71d9c1e6d0a71ca54ffb48
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2021-01-13 18:20:39 +0300

    imports improved

--- common/src/main/java/com/noirix/domain/hibernate/HibernateRole.java
+++ common/src/main/java/com/noirix/domain/hibernate/HibernateRole.java
@@ -5,7 +5,16 @@ import com.noirix.domain.SystemRoles;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Setter
 @Getter
